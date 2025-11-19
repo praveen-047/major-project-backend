5faf976c0f20289c13657a2fdad391e8a35f28e8
praveen <116637585+praveen-047@users.noreply.github.com>
2025-11-19 21:22:55 +0530

    Fix CORS preflight for Render

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cors({
   ],
   credentials: true,
 }));
+app.options("*", cors());
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
